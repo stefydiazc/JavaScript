4b85dde8738272225a1a6215f08d25081fa88dc2
María Gabriela <gabrielarequenahojas@gmail.com>
2017-07-30 22:30:10 -0500

    index
    creacion del index

---
+++ proyectoInfantil/js/javascript.js
@@ -0,0 +1,281 @@
+$('document').ready(function()
+{ 		 		
+		 // name validation
+		  var nameregex = /^[a-zA-Z ]+$/;
+		 
+		 $.validator.addMethod("validname", function( value, element ) {
+		     return this.optional( element ) || nameregex.test( value );
+		 });
+    
+    
+    var availableTags = [
+     "Afghanistan",
+		"Albania",
+		"Algeria",
+		"Andorra",
+		"Angola",
+		"Antigua and Barbuda",
+		"Argentina",
+		"Armenia",
+		"Australia",
+		"Austria",
+		"Azerbaijan",
+		"Bahamas",
+		"Bahrain",
+		"Bangladesh",
+		"Barbados",
+		"Belarus",
+		"Belgium",
+		"Belize",
+		"Benin",
+		"Bhutan",
+		"Bolivia",
+		"Bosnia and Herzegovina",
+		"Botswana",
+		"Brazil",
+		"Brunei",
+		"Bulgaria",
+		"Burkina Faso",
+		"Burundi",
+		"Cambodia",
+		"Cameroon",
+		"Canada",
+		"Cape Verde",
+		"Central African Republic",
+		"Chad",
+		"Chile",
+		"China",
+		"Colombia",
+		"Comoros",
+		"Congo (Brazzaville)",
+		"Congo",
+		"Costa Rica",
+		"Cote d'Ivoire",
+		"Croatia",
+		"Cuba",
+		"Cyprus",
+		"Czech Republic",
+		"Denmark",
+		"Djibouti",
+		"Dominica",
+		"Dominican Republic",
+		"East Timor (Timor Timur)",
+		"Ecuador",
+		"Egypt",
+		"El Salvador",
+		"Equatorial Guinea",
+		"Eritrea",
+		"Estonia",
+		"Ethiopia",
+		"Fiji",
+		"Finland",
+		"France",
+		"Gabon",
+		"Gambia, The",
+		"Georgia",
+		"Germany",
+		"Ghana",
+		"Greece",
+		"Grenada",
+		"Guatemala",
+		"Guinea",
+		"Guinea-Bissau",
+		"Guyana",
+		"Haiti",
+		"Honduras",
+		"Hungary",
+		"Iceland",
+		"India",
+		"Indonesia",
+		"Iran",
+		"Iraq",
+		"Ireland",
+		"Israel",
+		"Italy",
+		"Jamaica",
+		"Japan",
+		"Jordan",
+		"Kazakhstan",
+		"Kenya",
+		"Kiribati",
+		"Korea, North",
+		"Korea, South",
+		"Kuwait",
+		"Kyrgyzstan",
+		"Laos",
+		"Latvia",
+		"Lebanon",
+		"Lesotho",
+		"Liberia",
+		"Libya",
+		"Liechtenstein",
+		"Lithuania",
+		"Luxembourg",
+		"Macedonia",
+		"Madagascar",
+		"Malawi",
+		"Malaysia",
+		"Maldives",
+		"Mali",
+		"Malta",
+		"Marshall Islands",
+		"Mauritania",
+		"Mauritius",
+		"Mexico",
+		"Micronesia",
+		"Moldova",
+		"Monaco",
+		"Mongolia",
+		"Morocco",
+		"Mozambique",
+		"Myanmar",
+		"Namibia",
+		"Nauru",
+		"Nepa",
+		"Netherlands",
+		"New Zealand",
+		"Nicaragua",
+		"Niger",
+		"Nigeria",
+		"Norway",
+		"Oman",
+		"Pakistan",
+		"Palau",
+		"Panama",
+		"Papua New Guinea",
+		"Paraguay",
+		"Peru",
+		"Philippines",
+		"Poland",
+		"Portugal",
+		"Qatar",
+		"Romania",
+		"Russia",
+		"Rwanda",
+		"Saint Kitts and Nevis",
+		"Saint Lucia",
+		"Saint Vincent",
+		"Samoa",
+		"San Marino",
+		"Sao Tome and Principe",
+		"Saudi Arabia",
+		"Senegal",
+		"Serbia and Montenegro",
+		"Seychelles",
+		"Sierra Leone",
+		"Singapore",
+		"Slovakia",
+		"Slovenia",
+		"Solomon Islands",
+		"Somalia",
+		"South Africa",
+		"Spain",
+		"Sri Lanka",
+		"Sudan",
+		"Suriname",
+		"Swaziland",
+		"Sweden",
+		"Switzerland",
+		"Syria",
+		"Taiwan",
+		"Tajikistan",
+		"Tanzania",
+		"Thailand",
+		"Togo",
+		"Tonga",
+		"Trinidad and Tobago",
+		"Tunisia",
+		"Turkey",
+		"Turkmenistan",
+		"Tuvalu",
+		"Uganda",
+		"Ukraine",
+		"United Arab Emirates",
+		"United Kingdom",
+		"United States",
+		"Uruguay",
+		"Uzbekistan",
+		"Vanuatu",
+		"Vatican City",
+		"Venezuela",
+		"Vietnam",
+		"Yemen",
+		"Zambia",
+		"Zimbabwe"
+    ];
+    $( "#paises" ).autocomplete({
+      source: availableTags
+    });
+    
+		 // valid email pattern
+		 var eregex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		 
+		 $.validator.addMethod("validemail", function( value, element ) {
+		     return this.optional( element ) || eregex.test( value );
+		 });
+		 
+		 $("#register-form").validate({
+        framework: 'bootstrap4',
+        icon: {
+            valid: 'fa fa-check',
+            invalid: 'fa fa-times',
+            validating: 'fa fa-refresh'
+        },		
+		  rules:
+		  {
+				name: {
+					required: true,
+					validname: true,
+					minlength: 4
+				},
+                lastname: {
+					required: true,
+					validape: true,
+					minlength: 4
+				},
+				email: {
+					required: true,
+					validemail: true
+				},
+				password: {
+					required: true,
+					minlength: 8,
+					maxlength: 15
+				},
+				
+		   },
+		   messages:
+		   {
+				name: {
+					required: "Por favor, ingrese su nombre",
+					validname: "El nombre debe contener solo alfabetos y espacio",
+					minlength: "El nombre es muy corto"
+					  },
+                lastname: {
+					required: "Por favor, ingrese su apellido",
+					validape: "El apellido debe contener solo alfabetos y espacio",
+					minlength: "El apellido es muy corto"
+					  },
+			    email: {
+					  required: "Por favor, ingrese su correo",
+					  validemail: "Por favor, ingrese un correo valido"
+					   },
+				password:{
+					required: "Por favor, ingrese su contraseña",
+					minlength: "La contraseña debe tener minimo 8 caracteres"
+					},
+				
+		   },
+		   errorPlacement : function(error, element) {
+			  $(element).closest('.form-group').find('.help-block').html(error.html());
+		   },
+		   highlight : function(element) {
+			  $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+		   },
+		   unhighlight: function(element, errorClass, validClass) {
+			  $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+			  $(element).closest('.form-group').find('.help-block').html('');
+		   },
+	
+		   }); 
+});
