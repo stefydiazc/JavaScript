cf845795f4577df16f85acecded0b464c435a030
Stefy <stefydiazc@gmail.com>
2017-08-08 18:31:50 -0500

    Acceso a los datos del json
    Modificacion del json
    Creacion de la clase cuentos
    Parse de datos
    Obtencion de los datos en arreglos. usando las clases.

---
+++ kiara/js/03-start.js
@@ -0,0 +1,50 @@
+
+
+/*Funcion inicial al cargar la pagina*/
+$(function() {
+    setData();
+});
+
+function setData(){
+	objJSON = JSON.parse(datos);
+	contenido = [];
+	
+	console.log(objJSON);
+	
+	for (property in objJSON){
+		
+		switch(property){
+			case "contenido":
+			$.each(objJSON[property], function(index, value) {
+				
+				switch(Object.keys(value)[0]){
+			
+					case "cuento":
+	
+						$.each(value.cuento, function(index, value) {
+       
+							contenido.push(new Cuento(value));
+						});
+						break;
+					}
+			});
+				break;
+		
+	}
+				   
+console.log(contenido.length);
+console.log(contenido[0]);	
+console.log(contenido[2]);	
+}
+	
+		
+	}
+
+
+ 
+
+/*
+function setesfera(key, title, description, image){
+	$("")
+}
+*/
